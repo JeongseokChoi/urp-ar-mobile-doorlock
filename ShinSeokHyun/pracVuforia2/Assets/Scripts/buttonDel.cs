33c72ac4eeab6f6b8ddbf33d31fec80d53968f62
ShinSeokHyun <nihilz154@gmail.com>
2016-08-06 23:54:56 +0900

    button enable, log, color change when clicked

---
+++ ShinSeokHyun/pracVuforia2/Assets/Scripts/buttonDel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonDel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void onClick () {
+		Debug.Log ("button del clicked");
+	}
+}
