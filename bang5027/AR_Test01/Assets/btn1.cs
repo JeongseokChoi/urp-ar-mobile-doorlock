d81e8ce7f4fe34d09dba706f77a904644548924c
bang5027 <bang_ddd@naver.com>
2016-08-14 16:04:59 +0900

    2016.08.14 / 특정 마커에 번호판 띄우기

---
+++ bang5027/AR_Test01/Assets/btn1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class btn1 : MonoBehaviour {
+
+	Texture text2;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void onClick(){
+		Debug.Log ("1번 눌림");
+
+	}
+}
